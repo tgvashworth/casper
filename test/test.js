36eea2d95ba63cafd288fda26fe561fc129a48ed
Tom Ashworth <tom@phuu.net>
2013-03-29 01:31:23 +0000

    Tests with tap.

---
+++ test/test.js
@@ -0,0 +1,212 @@
+var capishe = require('../index'),
+    _ = require('underscore'),
+    test = require('tap').test,
+    sinon = require('sinon');
+
+// ==================================
+// Fixtures
+// ==================================
+
+var res = {};
+var setup = function () {
+  res.send = res.json = res.jsonp = sinon.spy(function () {
+    console.log.apply(console, [].slice.call(arguments));
+  });
+};
+setup();
+
+var app = {};
+app.get = app.post = function (path) {
+
+  var req = {};
+  req.params = [];
+  req.body = {
+    testKey: true
+  };
+
+  req.params = (path.match(/[a-zA-Z]+/g) || []).reduce(function (memo, param) {
+    memo[param] = true;
+    return memo;
+  }, {});
+
+  console.log(req.params);
+
+  var cbs = [].slice.call(arguments, 1),
+      doNext = true;
+  cbs.forEach(function (cb) {
+    if (!doNext) return;
+    doNext = false;
+    cb(req, res, function () {
+      console.log("next()");
+      doNext = true;
+    });
+  });
+};
+
+var fakeDoc = {};
+fakeDoc.save = function (cb) { return cb(null, this); };
+var FakeModel = function (obj) {
+  var doc = Object.create(fakeDoc);
+  return _.extend(doc, obj);
+};
+FakeModel.find = function () {
+  return {
+    exec: function (cb) {
+      cb(null, new FakeModel({ name: 'Fakey McFake' }));
+    }
+  };
+};
+FakeModel.findWithError = function () {
+  return {
+    exec: function (cb) {
+      cb(new Error("Shit broke"));
+    }
+  };
+};
+FakeModel.findWithNoResults = function () {
+  return {
+    exec: function (cb) {
+      cb(null, []);
+    }
+  };
+};
+FakeModel.findWithFalsyData = function () {
+  return {
+    exec: function (cb) {
+      cb(null, null);
+    }
+  };
+};
+
+// ==================================
+// Examples
+// ==================================
+
+// ==================================
+// General
+// ==================================
+test('general', function (t) {
+
+  t.test('noop with no data', function (t) {
+    setup();
+    app.get('/', capishe.noop());
+    t.ok(res.jsonp.calledOnce, 'jsonp was called');
+    t.ok(res.jsonp.calledWith({}), 'jsonp was called correct data');
+    t.end();
+  });
+
+  t.test('noop with no data', function (t) {
+    setup();
+    var data = { status: 'great' };
+    app.get('/', capishe.noop(data));
+    t.ok(res.jsonp.calledOnce, 'jsonp was called');
+    t.ok(res.jsonp.calledWith(data), 'jsonp was called correct data');
+    t.end();
+  });
+
+  t.end();
+
+});
+
+
+// ==================================
+// Database
+// ==================================
+test('database', function (t) {
+
+  t.test('sends data with 200', function (t) {
+    setup();
+    app.get('/', function (req, res) {
+      FakeModel
+        .find()
+        .exec(capishe.db(req, res));
+    });
+    t.ok(res.jsonp.calledOnce, 'jsonp was called once');
+    t.end();
+  });
+
+  t.test('sends 500 with errors', function (t) {
+    setup();
+    app.get('/', function (req, res) {
+      FakeModel
+        .findWithError()
+        .exec(capishe.db(req, res));
+    });
+    t.ok(res.jsonp.withArgs(500).calledOnce, '500 jsonp was called once');
+    t.end();
+  });
+
+  t.test('sends 404 with no reults', function (t) {
+    setup();
+    app.get('/', function (req, res) {
+      FakeModel
+        .findWithNoResults()
+        .exec(capishe.db(req, res));
+    });
+    t.ok(res.jsonp.withArgs(404, []).calledOnce, '404 jsonp was called once');
+    t.end();
+  });
+
+  t.test('sends 404 with no reults', function (t) {
+    setup();
+    app.get('/', function (req, res) {
+      FakeModel
+        .findWithFalsyData()
+        .exec(capishe.db(req, res));
+    });
+    t.ok(res.jsonp.withArgs(404, {}).calledOnce, '404 jsonp was called once');
+    t.end();
+  });
+
+  t.end();
+
+});
+
+// ==================================
+// Checks & filters
+// ==================================
+test('checks & filters', function (t) {
+
+  t.test('missing parameter is caught', function (t) {
+    setup();
+    app.get('/',
+            capishe.check.params('testParam'),
+            capishe.noop());
+    app.get('/:testParam',
+            capishe.check.params('testParam'),
+            capishe.noop());
+    t.ok(res.jsonp.withArgs(400).calledOnce, '400 jsonp was called once');
+    t.ok(res.jsonp.withArgs({}).calledOnce, '200 jsonp called once');
+    t.end();
+  });
+
+  t.test('present parameter is allowed', function (t) {
+    setup();
+    app.get('/',
+            capishe.check.body('fakeKey'),
+            capishe.noop());
+    app.get('/',
+            capishe.check.body('testKey'),
+            capishe.noop());
+    t.ok(res.jsonp.withArgs(400).calledOnce, '400 jsonp was called once');
+    t.ok(res.jsonp.withArgs({}).calledOnce, '200 jsonp called once');
+    t.end();
+  });
+
+  t.end();
+});
+
+app.get('/',
+        capishe.rm('testKey'),
+        capishe.noop());
+
+// ==================================
+// Logging
+// ==================================
+app.get('/:testParam',
+        capishe.log.the('params'),
+        capishe.noop());
+
+app.get('/:testParam',
+        capishe.log.the('params.testParam'),
+        capishe.noop());
